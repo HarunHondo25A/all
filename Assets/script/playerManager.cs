a0d24cf04d084de347da32b6674f11392038cc8e
DESKTOP-D5AAO38\user <harunkun13@gmail.com>
2025-12-06 08:31:32 +0900

    敵を追加 敵に当たるとプレイヤーの体力が1減る

--- Assets/script/playerManager.cs
+++ Assets/script/playerManager.cs
@@ -4,6 +4,7 @@ public class playerManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] float moveSpeed = 0.1f;
+    [SerializeField] float life = 5f;
 
 
     void Start()
@@ -29,6 +30,22 @@ public class playerManager : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0f, -moveSpeed * Time.deltaTime, 0f);
+        }
+        {
+            
+        }
+
+        if (life <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            life -= 1f;
         }
     }
 }
